4924c0a207d2cea674d8c74f2856f1a130578547
mauriciotl <mauriciotl77@gmail.com>
2026-07-13 19:17:29 -0600

    Chapter 09 - Improve RabittMQ setup and configuration

--- ch09/tacocloud-messaging-rabbitmq/src/main/java/tacos/messaging/MessagingConfig.java
+++ ch09/tacocloud-messaging-rabbitmq/src/main/java/tacos/messaging/MessagingConfig.java
@@ -1,5 +1,6 @@
 package tacos.messaging;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,11 @@ public class MessagingConfig {
   @Bean
   public Jackson2JsonMessageConverter messageConverter() {
     return new Jackson2JsonMessageConverter();
+  }
+
+  @Bean
+  public Queue orderQueue() {
+    return new Queue("tacocloud.order.queue", true); // durable = true
   }
 
 }
